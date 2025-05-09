134b595fb5848a9791d76c162675dca447794a6a
sofiaramirez <sofiaa.ramirez016@gmail.com>
2025-05-09 09:47:05 -0600

    Se crearon las rutas para la sección de Citas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppointmentsComponent } from './pages/appointments/appointments.component';
+import { ScheduleAppointmentComponent } from './pages/schedule-appointment/schedule-appointment.component';
+import { MyAppointmentsComponent } from './pages/my-appointments/my-appointments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'citas', component: AppointmentsComponent },
+  { path: 'agendar-citas', component: ScheduleAppointmentComponent },
+  { path: 'mis-citas', component: MyAppointmentsComponent },
+];
