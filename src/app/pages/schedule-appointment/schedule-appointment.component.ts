b569619086c225231376a438e07292b511f49118
sofiaramirez <sofiaa.ramirez016@gmail.com>
2025-05-09 09:40:22 -0600

    Se creo pagina de "agendar citas"

---
+++ src/app/pages/schedule-appointment/schedule-appointment.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { TimeButtonComponent } from '../../components/time-button/time-button.component';
+
+@Component({
+  selector: 'app-schedule-appointment',
+  imports: [TimeButtonComponent],
+  templateUrl: './schedule-appointment.component.html',
+})
+export class ScheduleAppointmentComponent {}
