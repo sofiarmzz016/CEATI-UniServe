706f1bf9c829ef3b6caf3d57126a4868b136ea0e
sofiarmzz016 <sofiaa.ramirez016@gmail.com>
2025-05-12 17:51:28 -0600

    Se agrega una validación al nav para que no se muestre en el inicio de sesion. Esto sucede en el `App.component.ts`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './base/navbar/navbar.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'UniServe';
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const currentRoute = event.urlAfterRedirects;
+        this.showNavbar = currentRoute !== '/iniciar-sesion';
+      });
+  }
 }
